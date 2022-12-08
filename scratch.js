8a3b3c63398ec853bbca62d9e5ca94220660b245
anymousuer293 <86797347+anymousuer293@users.noreply.github.com>
2022-12-07 18:25:47 -0600

    making it work with turbowarp

--- scratch.js
+++ scratch.js
@@ -1,30 +1,10 @@
-class StrictEqualityExtension {
-  getInfo() {
-    return {
-      id: 'something', // change this if you make an actual extension!
-      name: 'Strings',
-      blocks: [
-        {
-          opcode: 'strictlyEquals',
-          blockType: Scratch.BlockType.BOOLEAN,
-          text: '[ONE] strictly equals [TWO]',
-          arguments: {
-            ONE: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'First value'
-            },
-            TWO: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'Second value'
-            }
-          }
-        }
-      ]
-    };
+// NEW:
+(function(Scratch) {
+  'use strict';
+  class MyExtension {
+    getInfo () {
+      return { /* ... */ };
+    }
   }
-  strictlyEquals(args) {
-    // Note strict equality: Inputs must match exactly: in type, case, etc.
-    return args.ONE === args.TWO;
-  }
-}
-Scratch.extensions.register(new StrictEqualityExtension());
+  Scratch.extensions.register(new MyExtension());
+})(Scratch);
